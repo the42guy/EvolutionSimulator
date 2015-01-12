222cc3ac8d47292108d26325fa36d556954cdd0c
the42guy <peter@alternateuniverse.cu.cc>
2015-01-12 15:20:46 +0530

    Content provided
    The EvoSim project source files provided.

---
+++ src/EvoSimDriver.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Peter on 12/7/2014.
+ */
+
+/**
+ * Developers are Peter and Amol
+ */
+public class EvoSimDriver {
+    public static void main(String[] args) {
+        EvoSimDriver.getOption();
+    }
+
+    private static void getOption() {
+        try {
+            MonoCreature mc1 = new MonoCreature("Q", "Q");                                                                  //A monocreature (with two genes of one character
+            MonoCreature mc2 = new MonoCreature("q", "q");                                                                  //Another one
+            MonoHybrid m = new MonoHybrid(3);
+            m.seedParents(mc1, mc2);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Wrong genotype entered!");
+        } finally {
+            System.out.println("\n\nEnd of program");
+        }
+    }
+}
