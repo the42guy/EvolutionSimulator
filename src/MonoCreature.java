b9dff406f77ab80567139cd77a5a64d98d55331b
the42guy <peter@alternateuniverse.cu.cc>
2015-01-24 20:24:17 +0530

    Developing it more...

--- src/MonoCreature.java
+++ src/MonoCreature.java
@@ -9,6 +9,7 @@ public class MonoCreature {
     private String geneMakeup;                                                                                          //The make-up of the gene
     ArrayList<Character> genes = new ArrayList<Character>(2);
     private String gamete1, gamete2;
+    ArrayList<MonoCreature> fusedWith = new ArrayList<MonoCreature>();
 
     public MonoCreature(char characterGene1, char characterGene2) {
         this.genes.add(characterGene1);
@@ -16,9 +17,8 @@ public class MonoCreature {
         if(!geneCheck()) {
             throw new IllegalArgumentException("Genotype error");
         }
-
+        this.setDominantTrait();
     }
-
     private boolean geneCheck() {                                                                                       //Checks integrity of genes
         boolean isOK = false;
         int allVerified = 0;
@@ -27,8 +27,7 @@ public class MonoCreature {
         System.out.println(gene1 + gene2);
         return isOK;
     }
-
-    private void setDominantTrait() {
+    private void setDominantTrait() {                                                                                   //Sets the dominant trait
         char domTrait;
         for(int i = 0; i < 2; i++) {
             char geneThisIteration = this.genes.get(0);
@@ -36,7 +35,20 @@ public class MonoCreature {
                 domTrait = geneThisIteration;
             }
         }
-
+    }
+    private void createGametes() {                                                                                      //Initializes the gametes for later use
+        gamete1 = genes.get(0).toString();
+        gamete2 = genes.get(1).toString();
+    }
+    protected String getGamete(int whichOne) {                                                                          //Gets the required gamete
+        String gamete = "";
+        if((whichOne==0) || (whichOne==1)) {
+            gamete = genes.get(whichOne).toString();
+        }
+        return gamete;
+    }
+    protected void fusedWith(MonoCreature aCreature) {
+        this.fusedWith.add(aCreature);
     }
 }
 
