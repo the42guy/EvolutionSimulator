7e825caed3bb4fa3604fc503baf63d55a25af73e
the42guy <peter@alternateuniverse.cu.cc>
2015-01-24 22:28:26 +0530

    Time to recreate the MonoHybrid as well

--- src/MonoHybrid.java
+++ src/MonoHybrid.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
+
 /**
  * Created by Peter on 1/24/2015.
  */
 public class MonoHybrid {
     int maxGenerations;
-
+    private MonoCreature[] seeds = new MonoCreature[2];
+    ArrayList<MonoCreature> bufferList = new ArrayList<MonoCreature>();
+    ArrayList<ArrayList<MonoCreature>> totalList = new ArrayList<ArrayList<MonoCreature>>();
     public MonoHybrid(int generations) {
         this.maxGenerations = generations;
     }
-
     protected void seedParents(MonoCreature mc1, MonoCreature mc2) {
-
+        seeds[0] = mc1;
+        seeds[1] = mc2;
+        bufferList.add(seeds[0]);
+        bufferList.add(seeds[1]);
     }
 }
