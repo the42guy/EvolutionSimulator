5dd3e6d604cd54252397dd04c02fedf922f2ebf1
the42guy <peter@alternateuniverse.cu.cc>
2015-01-24 17:22:43 +0530

    Developing it more...

--- src/MonoCreature.java
+++ src/MonoCreature.java
@@ -7,30 +7,36 @@ public class MonoCreature {
     private String dominantTrait;                                                                                       //The dom trait for the creature
     private String genotypeCharacter;                                                                                   //The character representing the gene
     private String geneMakeup;                                                                                          //The make-up of the gene
-    ArrayList<String> genes = new ArrayList<String>(2);
+    ArrayList<Character> genes = new ArrayList<Character>(2);
     private String gamete1, gamete2;
 
-    public MonoCreature(String characterGene1, String characterGene2) {
+    public MonoCreature(char characterGene1, char characterGene2) {
         this.genes.add(characterGene1);
         this.genes.add(characterGene2);
+        if(!geneCheck()) {
+            throw new IllegalArgumentException("Genotype error");
+        }
+
     }
 
     private boolean geneCheck() {                                                                                       //Checks integrity of genes
         boolean isOK = false;
         int allVerified = 0;
-        for(int i = 0; i < 2; i++) {                                                                                    //Iterates over two genes and increases verif count for each success
-            int length = genes.get(i).length();
-            if(length == 1) {
-                allVerified++;
+        String gene1 = genes.get(0).toString();
+        String gene2 = genes.get(1).toString();
+        System.out.println(gene1 + gene2);
+        return isOK;
+    }
+
+    private void setDominantTrait() {
+        char domTrait;
+        for(int i = 0; i < 2; i++) {
+            char geneThisIteration = this.genes.get(0);
+            if(Character.isUpperCase(geneThisIteration)) {
+                domTrait = geneThisIteration;
             }
         }
-        if(genes.get(0).equalsIgnoreCase(genes.get(1))) {                                                               //If both are equal, another increase
-            allVerified++;
-        }
-        if(allVerified == 3) {                                                                                          //makes it true
-            isOK = true;
-        }
-        return isOK;
+
     }
 }
 
